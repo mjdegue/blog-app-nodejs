9d25e57f8c4304675043f6996c5440e5acc0b8b3
maximiliano degue <maximiliano.degue@gmail.com>
2025-05-27 21:47:02 +0200

    Added basic navigation, and a basic submit without backend functionality

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const Config = {
-    PORT: 3000,
-}
+  PORT: 3000,
+  BODY_PARSER: bodyParser.urlencoded({ extended: true }),
+};
 
 const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+  res.render("index.ejs", { isPost: false });
+});
+
+app.get("/create", (req, res) => {
+  res.render("create.ejs", { isPost: false });
+});
+
+app.post("/submit", Config.BODY_PARSER, (req, res) => {
+  console.log(req.body);
+  res.send(201);
 });
 
 app.listen(Config.PORT, (err) => {
-    if (err) {
-        console.log("Error while initializing server:\n" + err);
-        return;
-    }
+  if (err) {
+    console.log("Error while initializing server:\n" + err);
+    return;
+  }
 
-    console.log(`Server Ready: http://localhost:${Config.PORT}/`);
+  console.log(`Server Ready: http://localhost:${Config.PORT}/`);
 });
